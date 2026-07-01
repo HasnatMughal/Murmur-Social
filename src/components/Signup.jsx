66330b0236ae6315b71f315cb94e269738cfe9ea
TDF Hasnat <mughasnat377@gmail.com>
2026-07-01 08:16:01 +0500

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
             name, email, password, 
           }) 
           if(userAccount){
-           navigate("/feed")
+           navigate("/")
           }
         } catch (error) {
             console.log(error);
