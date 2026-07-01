830fb71b586e14e0a59689237106b13d4ca91f52
TDF Hasnat <mughasnat377@gmail.com>
2026-07-01 08:30:12 +0500

    Responsiveness

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,10 @@ function LoginPage() {
   
        <div className='flex flex-row  w-full justify-center bg-gray-50 h-screen'>
 
-        <div className='w-1/2 h-full '>
-        <img src={signUpPic} className='w-full h-full object-cover' alt="" />
+        <div className='md:w-1/2 h-full '>
+        <img src={signUpPic} className='hidden md:block w-full h-full object-cover' alt="" />
         </div>
-          <div className='w-1/2 flex items-center justify-center h-screen'>
+          <div className='w-full   md:w-1/2 flex items-center justify-center h-screen'>
 <Login />
           </div>
         </div>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,10 +6,10 @@ function SignUpPage() {
   return (
     <div className='flex flex-row  w-full justify-center bg-gray-50 min-h-screen h-screen '>
 
-        <div className='w-1/2 h-full '>
-        <img src={signUpPic} className='w-full h-full object-cover' alt="" />
+        <div className=' md:w-1/2 h-full '>
+        <img src={signUpPic} className='hidden md:block w-full h-full object-cover' alt="" />
         </div>
-          <div className='w-1/2 flex items-center justify-center h-screen'>
+          <div className=' w-full  md:w-1/2 flex items-center justify-center h-screen'>
 <Signup />
           </div>
         
